0a134e5373786c1a8623d142fa00e0c111ca396c
Ruben <Romo>
2019-04-18 15:41:34 -0500

    Buttons added

--- app/src/main/java/com/rubenjromo/lcrdicegame/MainActivity.kt
+++ app/src/main/java/com/rubenjromo/lcrdicegame/MainActivity.kt
@@ -54,12 +54,12 @@ class MainActivity : AppCompatActivity() {
     fun rollDice(){
         val randomInt = Random.nextInt(6) + 1
         val drawableResource = when (randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.dice_dot
+            2 -> R.drawable.dice_dot
+            3 -> R.drawable.dice_dot
+            4 -> R.drawable.dice_l
+            5 -> R.drawable.dice_c
+            else -> R.drawable.dice_r
         }
         dice_image.setImageResource(drawableResource)
     }
