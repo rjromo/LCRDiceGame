c6a944a3f4f36fba158c3f44f5d9175a2adc95e6
Ruben <Romo>
2019-04-18 10:19:56 -0500

    AndroidX

--- app/src/main/java/com/rubenjromo/lcrdicegame/MainActivity.kt
+++ app/src/main/java/com/rubenjromo/lcrdicegame/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.rubenjromo.lcrdicegame
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
